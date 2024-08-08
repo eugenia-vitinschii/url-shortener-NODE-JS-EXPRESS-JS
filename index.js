6389e1f86a1b23c2ed27718e444d30799ac7aaa9
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2024-08-08 22:00:53 +0300

    create urlSchema, add contollers, routes

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 
+const { connectToMongoDB } = require("./connect");
 
-const { connectToMongoDB } = require("./connect")
+const urlRoute = require("./routes/url");
+const URL = require("./models/url")
+
 
 const app = express();
 const PORT = 8001;
 
-connectToMongoDB("mongodb://localhost:27017/nodejs-short-url").then( ()=>
-console.log("Mongo DB connected!"))
+connectToMongoDB("mongodb://localhost:27017/nodejs-short-url").then(() =>
+  console.log("Mongo DB connected!")
+);
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/url", urlRoute);
+
+app.get("/home", (req, res) => {
+  return res.render("home");
+});
 
 app.get("/", (req, res) => {
-   return res.send("Hello from Page!")
-})
+  return res.send("Hello from Page!");
+});
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: {
+          timeStamp: Date.now(),
+        },
+      },
+    }
+  );
+  res.redirect(entry.redirectURL);
+});
 
-
-app.listen(PORT, () => console.log(` Server Started at PORT: ${PORT}`));
+app.listen(PORT, () => console.log(`Server Started at PORT: ${PORT}`));
